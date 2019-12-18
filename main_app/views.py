a5cff3d566397845a99f394c482f054f7cc49e25
Jon <jon@Sciences-MacBook-Pro.local>
2019-12-18 14:11:10 -0800

    set up base page setup

--- main_app/views.py
+++ main_app/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('<h1>Hello</h1>')
+
+def landing(request):
+    return render(request, 'index.html')
+
+def auth(request):
+    return render(request, 'auth.html')
+
